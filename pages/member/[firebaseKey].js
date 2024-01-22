2c3bb192412dc0072762deb5d49370c59adfd89d
Eric Frey <ericlfrey@gmail.com>
2024-01-22 16:28:24 -0600

    The ViewMemberPage shows the image, name, and role of the Team Member. Ready to push.

--- pages/member/[firebaseKey].js
+++ pages/member/[firebaseKey].js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
@@ -14,8 +15,16 @@ export default function ViewMemberPage() {
   }, []);
 
   return (
-    <>
-      <h1>{member.name}</h1>
-    </>
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="d-flex flex-column">
+        <img src={member.image} alt={member.name} style={{ width: '300px' }} />
+      </div>
+      <div className="text-dark ms-5 details">
+        <h5>
+          {member.name}
+        </h5>
+        <p>{member.role}</p>
+      </div>
+    </div>
   );
 }
