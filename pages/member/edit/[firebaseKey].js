c16c3632413c9877436ab5188cc28be458a1594a
Eric Frey <ericlfrey@gmail.com>
2024-01-22 17:06:59 -0600

    Added the Member form to the dynamic EditMemberPage. Need to update logic in MemberForm to handle Update.

--- pages/member/edit/[firebaseKey].js
+++ pages/member/edit/[firebaseKey].js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import MemberForm from '../../../components/MemberForm/MemberForm';
+import { getSingleMember } from '../../../api/memberData';
 
 export default function EditMemberPage() {
+  const [member, setMember] = useState({});
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    getSingleMember(firebaseKey).then(setMember);
+  }, []);
   return (
-    <div>EditMemberPage</div>
+    <MemberForm memberObj={member} />
   );
 }
